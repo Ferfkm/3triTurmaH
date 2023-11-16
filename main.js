016e5aca8f724a8314b372d2f01209c391f1723e
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2023-11-16 18:57:26 -0300

    onkeydown

--- main.js
+++ main.js
@@ -12,4 +12,7 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     tecla.onclick = function (){
         tocaSom(idAudio);
     }
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
+    }
 }
