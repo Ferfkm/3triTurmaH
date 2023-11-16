75fc30d12ec76116a747a299fba8b243843ac209
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2023-11-16 17:42:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,22 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-while(contador < listaDeTeclas.length){
-    const tecla =  listaDeTeclas[contador];  
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1];
     const idAudio = `#som_${efeito}`;
-    tecla.onclick = function(){
+
+    tecla.onclick = function (){
         tocaSom(idAudio);
     }
-    contador = contador + 1;
+
+    tecla.onkeydown = function(evento){
+        if(evento.code != 'Tab'){
+            tecla.classList.add('ativa')
+        }
+        
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
 }
